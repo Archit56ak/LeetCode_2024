9c78de478f98f014e0e3179dc4f732633d6d6295
Archit Vishwajit Kothawade <116948812+Archit56ak@users.noreply.github.com>
2024-02-19 10:33:27 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (49.03%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -3,24 +3,26 @@ public:
     bool isPowerOfTwo(int n) {
         bool flag = false;
         
-        while(n>0)
-        {
-            if(n%2==0)
-            {
-                n=n/2;
-                flag = true;
-            }
-            else if(n==1)
-            {
-                flag=true;
-                break;
-            }
-            else if(n%2!=0)
-            {
-                flag=false;
-                break;
-            }
-        }
-        return flag;
+//         while(n>0)
+//         {
+//             if(n%2==0)
+//             {
+//                 n=n/2;
+//                 flag = true;
+//             }
+//             else if(n==1)
+//             {
+//                 flag=true;
+//                 break;
+//             }
+//             else if(n%2!=0)
+//             {
+//                 flag=false;
+//                 break;
+//             }
+//         }
+//         return flag;
+        
+        return n>0 && not (n&n-1);
     }
 };
