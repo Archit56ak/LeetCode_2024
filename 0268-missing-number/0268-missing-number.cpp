def72db9ecd206f06bc3007d0b52f242708f7a90
Archit Vishwajit Kothawade <116948812+Archit56ak@users.noreply.github.com>
2024-02-20 19:22:59 +0530

    Time: 14 ms (63.35%), Space: 20.9 MB (16.09%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,12 +2,32 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) 
     {
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++)
-        {
-            if(i!=nums[i])
-                return i;
+        // Method 1
+        // sort(nums.begin(),nums.end());
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     if(i!=nums[i])
+        //         return i;
+        // }
+        // return nums.size();
+
+        //Method 2
+        int n = nums.size();
+        vector<int>v(n+1,-1);
+        for(int i =0;i<nums.size();i++){
+            v[nums[i]] = nums[i];
         }
-        return nums.size();
+        for(int i =0;i<v.size();i++){
+            if(v[i]==-1)return i;
+        }
+
+        //Method 3
+        // int sum = 0;
+        // for(int i=0;i<=nums.size();i++)
+        //     sum+=i;
+        // for(int i=0;i<nums.size();i++)
+        //     sum-=nums[i];
+        // return sum;
+        return 0;
     }
 };
