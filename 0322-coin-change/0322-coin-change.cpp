43ce5891f805f3a610b0a6c7b6aa70026718d66b
Archit Vishwajit Kothawade <116948812+Archit56ak@users.noreply.github.com>
2024-03-04 11:49:45 +0530

    Time: 30 ms (69.36%), Space: 16.1 MB (52.53%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int solve(vector<int> &coins,int amount,vector<int> &dp)
+    {
+        if(amount==0)
+            return 0;
+        if(amount<0)
+            return INT_MAX;
+        
+        if(dp[amount]!=-1)
+            return dp[amount];
+        
+        int min_val = INT_MAX;
+        for(int  i=0;i<coins.size();i++)
+        {
+            int ans = solve(coins,amount-coins[i],dp);
+            if(ans!=INT_MAX)
+                min_val = min(min_val,1+ans);
+        }
+        dp[amount] = min_val;
+        return min_val;
+    }
+    
+    int solveTab(vector<int> &coins,int amount)
+    {
+        vector<int> dp(amount+1,INT_MAX);
+        dp[0] = 0;
+        
+        for(int i=1;i<=amount;i++)
+        {
+            // Trying to  solve for every amount from 1 to amount
+            for(int j=0;j<coins.size();j++)
+            {
+                if(i-coins[j] >=0 && dp[i-coins[j]]!=INT_MAX)
+                dp[i] = min(dp[i],1+dp[i-coins[j]]);
+            }
+        }
+        if(dp[amount]==INT_MAX)
+            return -1;
+        return dp[amount]; 
+    }
+    int coinChange(vector<int>& coins, int amount) 
+    {
+        // int n = coins.size();    
+        // vector<int> dp(amount+1,-1);
+        // for(int i=0;i<n;i++)
+        // {
+        //     dp[coins[i]] = 1;
+        // }
+        int min_val = solveTab(coins,amount);
+        if(min_val == INT_MAX)
+            return -1;
+        else
+            return min_val;
+    }
+};
