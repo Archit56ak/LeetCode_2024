fdc41d847345450f04fd96968f0a1ce3c343dcbd
Archit Vishwajit Kothawade <116948812+Archit56ak@users.noreply.github.com>
2024-03-07 10:37:33 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (66.29%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -12,21 +12,34 @@ class Solution {
 public:
     ListNode* middleNode(ListNode* head) 
     {
-        //Length of whole linked list
-        ListNode *temp = head;
-        ListNode *midNode = head;
-        int len = 0,index = 0;
-        while(temp != NULL)
+        //Brute Force Method 
+        // //Length of whole linked list
+        // ListNode *temp = head;
+        // ListNode *midNode = head;
+        // int len = 0,index = 0;
+        // while(temp != NULL)
+        // {
+        //     len++;
+        //     temp=temp->next;
+        // }
+        // //Calculate the middle node Index
+        // index = (len/2)+1;
+        // for(int i=1;i<index;i++)
+        // {
+        //     midNode = midNode->next;
+        // }
+        // return midNode;
+        
+        
+        ListNode *fast = head;
+        ListNode *slow = head;
+        
+        while(fast!=NULL && fast->next!=NULL)
         {
-            len++;
-            temp=temp->next;
+            fast = fast->next->next;
+            slow = slow->next;
         }
-        //Calculate the middle node Index
-        index = (len/2)+1;
-        for(int i=1;i<index;i++)
-        {
-            midNode = midNode->next;
-        }
-        return midNode;
+        
+        return slow;
     }
 };
