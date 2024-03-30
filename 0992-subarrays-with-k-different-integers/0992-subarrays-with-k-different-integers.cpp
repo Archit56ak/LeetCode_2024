7a3e7e27e2a7737c8ad0d53c4d809995f25cc5cf
Archit Vishwajit Kothawade <116948812+Archit56ak@users.noreply.github.com>
2024-03-30 10:52:08 +0530

    Time: 126 ms (24.32%), Space: 52.3 MB (21.71%) - LeetHub

---
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int subarraysWithKDistinct(vector<int>& nums, int k) 
+    {
+        int max_k = SubArrayWithAtmostK(nums,k);
+        int reduced_max_k = SubArrayWithAtmostK(nums,k-1);
+        return (max_k - reduced_max_k);
+    }
+    
+    int SubArrayWithAtmostK(vector<int> nums, int k)
+    {
+        unordered_map<int,int> mpp;
+        int ans = 0;
+        int left=0,right=0;
+        while(right<nums.size())
+        {
+            mpp[nums[right]]++;
+            while(mpp.size()>k)
+            {
+                mpp[nums[left]]--;
+                if(mpp[nums[left]]==0) mpp.erase(nums[left]);
+                left++;
+            }
+            ans += right-left+1;
+            right++;
+        }
+        return ans;
+    }
+};
