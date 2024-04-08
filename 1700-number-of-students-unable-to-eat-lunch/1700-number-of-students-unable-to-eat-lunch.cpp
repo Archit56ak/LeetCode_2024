66621d79c85a5fa9845602908db42763e3eff5d5
Archit Vishwajit Kothawade <116948812+Archit56ak@users.noreply.github.com>
2024-04-09 00:21:27 +0530

    Time: 5 ms (31.29%), Space: 10.8 MB (50.25%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
@@ -2,20 +2,44 @@ class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) 
     {
-        vector<int> counts(2,0);
-        for(int s : students)
-            counts[s]++;
+//         vector<int> counts(2,0);
+//         for(int s : students)
+//             counts[s]++;
         
-        int ans = sandwiches.size();
-        for(int s : sandwiches)
-        {
-            if(counts[s]==0) break;
-            else if(ans==0) break;
+//         int ans = sandwiches.size();
+//         for(int s : sandwiches)
+//         {
+//             if(counts[s]==0) break;
+//             else if(ans==0) break;
             
-            counts[s]--;
-            ans--;
+//             counts[s]--;
+//             ans--;
+//         }
+        
+//         return ans;
+        
+        queue<int> S;
+        for(int s : students)
+            S.push(s);
+        
+        int ans = 0;
+        int i= 0 ;
+        while(!S.empty() && ans!=S.size())
+        {
+            if(S.front()==sandwiches[i])
+            {
+                S.pop();
+                i++;
+                ans = 0;
+            }
+            else
+            {
+                S.push(S.front());
+                S.pop();
+                ans++;
+            }
         }
         
-        return ans;
+        return S.size();
     }
 };
