4cad70d91c0f8f743139fa3c16db902aabf898ca
Archit Vishwajit Kothawade <116948812+Archit56ak@users.noreply.github.com>
2024-04-09 22:35:22 +0530

    Time: 0 ms (100.00%), Space: 9.3 MB (67.56%) - LeetHub

---
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) 
+    {
+        int time = 0;
+        while(tickets[k]!=0)
+        {
+            for(int i=0;i<tickets.size();i++)
+            {
+                if(tickets[i]!=0 && tickets[k]!=0)
+                {
+                     tickets[i]--;
+                     time++;
+                }
+            }
+        }
+        
+        return time;
+    }
+};
