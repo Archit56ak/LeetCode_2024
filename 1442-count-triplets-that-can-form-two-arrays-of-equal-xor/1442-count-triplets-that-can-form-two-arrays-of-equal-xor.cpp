9e003553049708fd04d52e5b344021f5330f9562
Archit Vishwajit Kothawade <116948812+Archit56ak@users.noreply.github.com>
2024-05-30 11:17:27 +0530

    Time: 66 ms (5.12%), Space: 8.9 MB (86.41%) - LeetHub

---
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int countTriplets(vector<int>& arr) 
+    {
+        int cnt = 0;
+        // if(arr.size()<2)
+        //     return 0;
+        
+        
+        for(int i=0;i<arr.size();i++)
+        {
+            for(int j=i+1;j<arr.size();j++)
+            {
+                int a = 0;
+                for(int k=i;k<j;k++)
+                {
+                    a^=arr[k];
+                }
+                
+                int b= 0;
+                for(int k=j;k<arr.size();k++)
+                {
+                    b^=arr[k];
+                    if(a==b)
+                    cnt++;
+                }
+                
+                
+            }
+        }
+        return cnt;
+    }
+};
