dc28a4a5991e66c175261dd5736271203af2420b
Archit Vishwajit Kothawade <116948812+Archit56ak@users.noreply.github.com>
2024-06-03 18:17:46 +0530

    Time: 19 ms (59.76%), Space: 11.8 MB (83.42%) - LeetHub

--- 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.cpp
+++ 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.cpp
@@ -2,12 +2,9 @@ class Solution {
 public:
     int appendCharacters(string s, string t) 
     {
-        int s_len = s.length();
-        int t_len = t.length();
-        int ans = 0;
         int j = 0,i=0;
         int max_len=0;
-        while(i<s_len)
+        while(i<s.length())
         {
             if(s[i]==t[j])
             {
@@ -19,6 +16,6 @@ public:
             
             i++;
         }
-        return t_len-j;
+        return t.length()-j;
     }
 };
