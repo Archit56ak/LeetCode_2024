3727bea56316095cfab80132231436e27c690246
Archit Vishwajit Kothawade <116948812+Archit56ak@users.noreply.github.com>
2025-04-15 21:33:41 +0530

    Time: 3 ms (35.52%), Space: 62.9 MB (78.72%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int max_vol = 0;
+        int max_area = 0;
         int i = 0;
         int j = height.size()-1;
         while(i<j)
         {
-            int h = 0, l = 0;
+            max_area = max(min(height[i],height[j])*(j-i),max_area);
             if(height[i]<=height[j]) {
-                h = height[i];
                 i++;
             }
             else {
-                h = height[j];
                 j--;
             }
-            l = j-i+1;
-            cout<<"Height : "<<h<<" Length : "<<l<<endl;
-            int vol = h*l;
-            max_vol = max(max_vol,vol);
         }
-        return max_vol;
+        return max_area;
     }
 };
